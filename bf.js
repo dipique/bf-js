6416f46729922c0412befe21d2c6cdea8747605e
Daniel Kaschel <dipique@gmail.com>
2020-05-14 14:13:11 -0500

    Fix incorrect handling of nulls

--- bf.js
+++ bf.js
@@ -5,20 +5,20 @@ const EOF_CODE = -1     //the value that signifies that input is no longer being
 const MAX_INSTRUCTIONS_EXECUTED = 1_000_000_000 //upper limit on instructions to avoid infinite loops,
                                                 //but should really just make compile() async and set a timeout
 
-const compile = (txt, getChar) => {
-    let data = new Uint8Array(DATA_LEN).fill(0),
+const compile = (txt, getChar, putChar) => {
+    let data = new Int8Array(DATA_LEN).fill(0),
         dataPtr = 0,
         instrPtr = 0,
         output = [], //built one character at a time, then returned at the end joined as a string
         instructionCount = 0 //my one concession to semi-proactive error handling; infinite loop detection
     const getData = idx => data[idx || dataPtr]
-
+    const putChars = str => Array.from(str).map(ch => putChar(ch))
     const instructions = {
         '>': () => dataPtr++,
         '<': () => dataPtr--,
         '+': () => data[dataPtr]++,
         '-': () => data[dataPtr]--,
-        '.': () => output.push(String.fromCharCode(getData())),
+        '.': () => putChar(String.fromCharCode(Math.max(0, getData()))),
         ',': () => data[dataPtr] = charToAscii(getChar() || EOF_CODE),
         '[': () => { if (getData() === 0) instrPtr = findMatchingBrace(txt, instrPtr) },
         ']': () => { if (getData() !== 0) instrPtr = findMatchingBrace(txt, instrPtr) }
@@ -35,12 +35,12 @@ const compile = (txt, getChar) => {
     } catch(e) {
         return `CRASHED @ CHAR ${instrPtr}: '${e}'`
     }
-    return output.join('')
+    return '' //successful
 }
 
 //if the input is a string, get an ascii code; otherwise, just pass back the return.
 //This logic is to allow ascii codes provided directly (like EOF) to pass through untouched.
-const charToAscii = ch => Math.min(127, typeof ch === 'string' ? ch.charCodeAt(0) : ch)
+const charToAscii = ch => Math.max(-128, Math.min(127, typeof ch === 'string' ? ch.charCodeAt(0) : ch))
 
 const findMatchingBrace = (txt, currentBraceIdx) => {
     const currentBrace = txt[currentBraceIdx]
